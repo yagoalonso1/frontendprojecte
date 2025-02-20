a23ffbdf73faa53f6f5c31ee99d75fa08be8abab
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-20 15:42:57 +0100

    add colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation ("androidx.navigation:navigation-compose:2.6.0")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.31.0-alpha")
